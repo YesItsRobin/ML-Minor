a9ea07a614714ff31ec4d7b30fce3b0afd6fdc86
RobinTollenaar <yesitsrobin@gmail.com>
2023-10-04 15:17:39 +0200

    k-nn

--- Assignment2.py
+++ Assignment2.py
@@ -3,7 +3,6 @@ import numpy as np
 from sklearn.svm import SVC
 from sklearn import preprocessing
 from sklearn.naive_bayes import GaussianNB
-from sklearn.experimental import enable_halving_search_cv as HalvingGridSearchCV
 df = pd.read_csv('card_transdata.csv', header = 0)
 
 # Preprocessing scaled data
@@ -19,25 +18,39 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score, confusion_matrix
 
-# X = df_scaled
-# X2 =df[['repeat_retailer','used_chip','used_pin_number','online_order']]
-# X = pd.concat([X, X2], axis=1)
-y = df['fraud']
-#
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-#
-# knn = KNeighborsClassifier(n_neighbors=5)
-# knn.fit(X_train, y_train)
-#
-# y_pred = knn.predict(X_test)
-#
-# accuracy = accuracy_score(y_test, y_pred)
-# confusion = confusion_matrix(y_test, y_pred)
-#
-# print("K-NN:")
-# print("Accuracy (scaled):", accuracy)
-# print("Confusion Matrix (scaled):")
-# print(confusion)
+X = df_scaled
+X2 = df[['repeat_retailer', 'used_chip', 'used_pin_number', 'online_order']]
+X = pd.concat([X, X2], axis=1)
+
+X = X.sample(frac=0.05) #using fraction of the dataset due to processing time
+y = df['fraud'].iloc[X.index]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Hyperparameter tuning
+k_range = np.arange(1, 20)
+param_grid = {'n_neighbors': k_range}
+
+knn = KNeighborsClassifier()
+grid_search = GridSearchCV(estimator=knn, param_grid=param_grid, cv=5, n_jobs=-1)
+
+grid_search.fit(X_train, y_train)
+best_k = grid_search.best_params_['n_neighbors']
+
+# Create a new KNN model with the best k
+best_knn = KNeighborsClassifier(n_neighbors=best_k)
+best_knn.fit(X_train, y_train)
+
+y_pred = best_knn.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+confusion = confusion_matrix(y_test, y_pred)
+
+print("K-NN:")
+print("Best k:", best_k)
+print("Accuracy (scaled):", accuracy)
+print("Confusion Matrix (scaled):")
+print(confusion)
 
 #Test with log transformed data
 
