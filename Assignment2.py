309bb40080e792c1e665f18cfd46534506ab32dc
rtollenaa <672023@student.inholland.nl>
2023-10-07 15:16:44 +0200

    K-NN improvement and authors
    Also removed unused code

--- Assignment2.py
+++ Assignment2.py
@@ -1,3 +1,8 @@
+'''
+Code by Robin Tollenaar and Finn Thomas
+07/10/2023
+'''
+
 import pandas as pd
 import numpy as np
 from sklearn.svm import SVC
@@ -53,67 +58,21 @@ print("Best k:", best_k)
 print("Accuracy (scaled):", accuracy)
 print("Confusion Matrix (scaled):")
 print(confusion)
+'''
+for k in range (1, 11, 2):
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
 
-#Test with log transformed data
+    accuracy = accuracy_score(y_test, y_pred)
+    confusion = confusion_matrix(y_test, y_pred)
 
-# X = df_log_transformed
-# X2 =df[['repeat_retailer','used_chip','used_pin_number','online_order']]
-# X = pd.concat([X, X2], axis=1)
-#
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-#
-# knn = KNeighborsClassifier(n_neighbors=5)
-# knn.fit(X_train, y_train)
-#
-# y_pred = knn.predict(X_test)
-#
-# accuracy = accuracy_score(y_test, y_pred)
-# confusion = confusion_matrix(y_test, y_pred)
-#
-# print("Accuracy (log data):", accuracy)
-# print("Confusion Matrix (log data):")
-# print(confusion)
-
-######### Training and testing Naive Bayes #########
-
-# X = df_scaled
-# X2 = df[['repeat_retailer', 'used_chip', 'used_pin_number', 'online_order']]
-# X = pd.concat([X, X2], axis=1)
-#
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-#
-# gnb = GaussianNB()
-# gnb.fit(X_train, y_train)
-#
-# y_pred = gnb.predict(X_test)
-#
-# accuracy = accuracy_score(y_test, y_pred)
-# confusion = confusion_matrix(y_test, y_pred)
-# print("Naive Bayes:")
-# print("Accuracy (scaled):", accuracy)
-# print("Confusion Matrix (scaled):")
-# print(confusion)
-
-#Test with log transformed data
-
-# X = df_log_transformed
-# X2 = df[['repeat_retailer', 'used_chip', 'used_pin_number', 'online_order']]
-# X = pd.concat([X, X2], axis=1)
-#
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-#
-# gnb = GaussianNB()
-# gnb.fit(X_train, y_train)
-#
-# y_pred = gnb.predict(X_test)
-#
-# accuracy = accuracy_score(y_test, y_pred)
-# confusion = confusion_matrix(y_test, y_pred)
-# print("Naive Bayes:")
-# print("Accuracy (log):", accuracy)
-# print("Confusion Matrix (log):")
-# print(confusion)
-
+    print("K-NN:")
+    print("k: ", k)
+    print("Accuracy (scaled):", accuracy)
+    print("Confusion Matrix (scaled):")
+    print(confusion)
+'''
 ######### Training and testing SVC #########
 
 X = df_scaled
@@ -151,120 +110,3 @@ print("Best gamma:", best_gamma)
 print("Accuracy (scaled):", accuracy)
 print("Confusion Matrix (scaled):")
 print(confusion)
-#
-# #Test with log transformed data
-
-# X = df_scaled
-# X2 = df[['repeat_retailer', 'used_chip', 'used_pin_number', 'online_order']]
-# X = pd.concat([X, X2], axis=1)
-#
-# X = X.sample(frac=0.05) #using half of the dataset due to processing time
-# y = df['fraud'].iloc[X.index]
-#
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-#
-# svm = SVC(kernel='rbf')
-# svm.fit(X_train, y_train)
-#
-# y_pred = svm.predict(X_test)
-#
-# accuracy = accuracy_score(y_test, y_pred)
-# confusion = confusion_matrix(y_test, y_pred)
-#
-# print ("SVC")
-# print("Accuracy (log):", accuracy)
-# print("Confusion Matrix (log):")
-# print(confusion)
-#
-# ######### Training and testing Decision Tree #########
-#
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn.model_selection import train_test_split
-# from sklearn.metrics import accuracy_score, confusion_matrix
-#
-# X = df_scaled
-# X2 = df[['repeat_retailer', 'used_chip', 'used_pin_number', 'online_order']]
-# X = pd.concat([X, X2], axis=1)
-# y = df['fraud']
-#
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-#
-# dt = DecisionTreeClassifier()
-# dt.fit(X_train, y_train)
-#
-# y_pred = dt.predict(X_test)
-#
-# accuracy = accuracy_score(y_test, y_pred)
-# confusion = confusion_matrix(y_test, y_pred)
-#
-# print ("Decision Tree")
-# print("Accuracy (scaled):", accuracy)
-# print("Confusion Matrix (scaled):")
-# print(confusion)
-#
-# #Test with log transformed data
-#
-# X = df_log_transformed
-# X2 = df[['repeat_retailer', 'used_chip', 'used_pin_number', 'online_order']]
-# X = pd.concat([X, X2], axis=1)
-#
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-#
-# dt = DecisionTreeClassifier()
-# dt.fit(X_train, y_train)
-#
-# y_pred = dt.predict(X_test)
-#
-# accuracy = accuracy_score(y_test, y_pred)
-# confusion = confusion_matrix(y_test, y_pred)
-#
-# print ("Decision Tree")
-# print("Accuracy (log):", accuracy)
-# print("Confusion Matrix (log):")
-# print(confusion)
-#
-# ######### Training and testing Random Forest #########
-#
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.model_selection import train_test_split
-# from sklearn.metrics import accuracy_score, confusion_matrix
-#
-# X = df_scaled
-# X2 = df[['repeat_retailer', 'used_chip', 'used_pin_number', 'online_order']]
-# X = pd.concat([X, X2], axis=1)
-#
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-#
-# rf = RandomForestClassifier()
-# rf.fit(X_train, y_train)
-#
-# y_pred = rf.predict(X_test)
-#
-# accuracy = accuracy_score(y_test, y_pred)
-# confusion = confusion_matrix(y_test, y_pred)
-#
-# print ("Random Forest")
-# print("Accuracy (scaled):", accuracy)
-# print("Confusion Matrix (scaled):")
-# print(confusion)
-#
-# #Test with log transformed data
-#
-# X = df_log_transformed
-# X2 = df[['repeat_retailer', 'used_chip', 'used_pin_number', 'online_order']]
-# X = pd.concat([X, X2], axis=1)
-#
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-#
-# rf = RandomForestClassifier()
-# rf.fit(X_train, y_train)
-#
-# y_pred = rf.predict(X_test)
-#
-# accuracy = accuracy_score(y_test, y_pred)
-# confusion = confusion_matrix(y_test, y_pred)
-#
-# print ("Random Forest")
-# print("Accuracy (log):", accuracy)
-# print("Confusion Matrix (log):")
-# print(confusion)
