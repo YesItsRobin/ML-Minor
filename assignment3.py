330b86d968b3a0a36d59e1a6f015b3fb99fef8d0
RobinTollenaar <yesitsrobin@gmail.com>
2023-10-09 12:14:33 +0200

    clustering

--- assignment3.py
+++ assignment3.py
@@ -38,4 +38,60 @@ plt.grid(True)
 plt.show()
 
 #Best was 10 clusters
+
+
+# K-means clustering on scaled data
+kmeans = KMeans(n_clusters=10, random_state=42)
+df_scaled['cluster'] = kmeans.fit_predict(df_scaled)
+
+df_filtered = df_scaled[df_scaled['cluster'] != 5]
+
+cluster_profiles = df_filtered.groupby(by='cluster')[['distance_from_home', 'distance_from_last_transaction', 'ratio_to_median_purchase_price']].mean()
+cluster_profiles.plot(kind='bar', figsize=(10, 6))
+cluster_count = df_filtered['cluster'].value_counts()
+print(cluster_count)
+plt.title('Cluster Profiles')
+plt.xlabel('Cluster')
+plt.ylabel('Mean Value')
+plt.xticks(rotation=0)
+plt.show()
 '''
+# Expectation Maximization Algorithm
+from sklearn.mixture import GaussianMixture
+
+# Initialize an empty list to store the log-likelihood scores
+log_likelihoods = []
+'''
+# Define the range of clusters you want to try
+num_clusters = range(1, 11)  # You can adjust this range as needed
+
+# Fit Gaussian Mixture Model clustering for each value of k
+for k in num_clusters:
+    print(k)
+    gmm = GaussianMixture(n_components=k, random_state=42)
+    gmm.fit(df_scaled)
+    log_likelihoods.append(gmm.score(df_scaled))
+
+# Plot the log-likelihoods scores
+plt.figure(figsize=(8, 6))
+plt.plot(num_clusters, log_likelihoods, marker='o', linestyle='-', color='b')
+plt.xlabel('Number of Clusters (k)')
+plt.ylabel('Log-likelihood')
+plt.title('GMM Log-likelihood vs. k')
+plt.grid(True)
+plt.show()
+'''
+#Best was 6 clusters
+gmm = GaussianMixture(n_components=6, random_state=42)
+gmm.fit(df_scaled)
+# show cluster profiles and size
+df_scaled['cluster'] = gmm.predict(df_scaled)
+cluster_profiles = df_scaled.groupby(by='cluster')[['distance_from_home', 'distance_from_last_transaction', 'ratio_to_median_purchase_price']].mean()
+cluster_profiles.plot(kind='bar', figsize=(10, 6))
+cluster_count = df_scaled['cluster'].value_counts()
+print(cluster_count)
+plt.title('Cluster Profiles')
+plt.xlabel('Cluster')
+plt.ylabel('Mean Value')
+plt.xticks(rotation=0)
+plt.show()
